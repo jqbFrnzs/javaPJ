94beaf61776a4c479fc38b2d2d43bbeb13fe62fb
Jqb Frnzs <jacobFz@tutanota.com>
2020-05-19 17:12:34 +0200

    Extracted code to handle client connection to handleClientSocket() method

--- ManagerServer/src/com/tm/ServerMain.java
+++ ManagerServer/src/com/tm/ServerMain.java
@@ -16,17 +16,21 @@ public class ServerMain {
                 System.out.println("Going to accept client connection just now");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
-                OutputStream outputStream = clientSocket.getOutputStream();
-                for (int i = 0; i < 10; i++) {
-                    outputStream.write(("Time: " + new Date() + "\n").getBytes());
-                    Thread.sleep(1000);
-                }
-                clientSocket.close();
+                handleClientSocket(clientSocket);
             }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for (int i = 0; i < 10; i++) {
+            outputStream.write(("Time: " + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
     }
 }
