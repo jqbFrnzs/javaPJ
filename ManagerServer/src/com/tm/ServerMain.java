1d1f5f8a598881cc08e8142abf2ba9b2ccde3600
Jqb Frnzs <jacobFz@tutanota.com>
2020-05-19 16:23:01 +0200

    Added simulation of single-thread server. Only one client can connect at the same time.

--- ManagerServer/src/com/tm/ServerMain.java
+++ ManagerServer/src/com/tm/ServerMain.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+
 
 public class ServerMain {
     public static void main(String[] args) throws IOException {
@@ -11,12 +13,19 @@ public class ServerMain {
         ServerSocket serverSocket = new ServerSocket(port);
         try {
             while (true) {
+                System.out.println("Going to accept client connection just now");
                 Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
                 OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("Hello my Client\n".getBytes());
+                for (int i = 0; i < 10; i++) {
+                    outputStream.write(("Time: " + new Date() + "\n").getBytes());
+                    Thread.sleep(1000);
+                }
                 clientSocket.close();
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
