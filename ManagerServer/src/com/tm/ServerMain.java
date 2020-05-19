fc8929d9ff94f0f3b215c4c9a28bceb60aaa1973
Jqb Frnzs <jacobFz@tutanota.com>
2020-05-19 17:27:27 +0200

    Created ServerWorker class that extends Thread to handle communication between server and client

--- ManagerServer/src/com/tm/ServerMain.java
+++ ManagerServer/src/com/tm/ServerMain.java
@@ -16,31 +16,13 @@ public class ServerMain {
                 System.out.println("Going to accept client connection just now");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
-                Thread t = new Thread() {
-                    @Override
-                    public void run() {
-                        try {
-                            handleClientSocket(clientSocket);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                t.start();
+                ServerWorker worker = new ServerWorker(clientSocket);
+                worker.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
-        OutputStream outputStream = clientSocket.getOutputStream();
-        for (int i = 0; i < 10; i++) {
-            outputStream.write(("Time: " + new Date() + "\n").getBytes());
-            Thread.sleep(1000);
-        }
-        clientSocket.close();
-    }
+
 }
