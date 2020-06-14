433fde8748bf4d4a8b8565d507c38764752c4bb9
Jqb Frnzs <jacobFz@tutanota.com>
2020-06-14 17:52:26 +0200

    Added initial layout for GUI

---
+++ ManagerClient/src/com/tm/UserListPane.java
@@ -0,0 +1,43 @@
+package com.tm;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UserListPane extends JPanel implements UserStatusListener {
+
+    private final ManagerClient client;
+
+    // show list of users
+    private JList<String> userListUI;
+
+    public UserListPane(ManagerClient client) {
+        this.client = client;
+        this.client.addUserStatusListener(this);
+
+        userListUI = new JList<>();
+        setLayout(new BorderLayout());
+        add(new JScrollPane(userListUI), BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        ManagerClient client = new ManagerClient("localhost", 8819);
+
+        UserListPane userListPane = new UserListPane(client);
+        JFrame frame = new JFrame("User List");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 600);
+
+        frame.getContentPane().add(userListPane, BorderLayout.CENTER);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void online(String login) {
+
+    }
+
+    @Override
+    public void offline(String login) {
+
+    }
+}
