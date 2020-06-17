16428fa60b5c787869048f37877b748fca6efe3f
Jqb Frnzs <jacobFz@tutanota.com>
2020-06-17 21:18:51 +0200

    Added basic layout for message pane.

--- ManagerClient/src/com/tm/MessagePane.java
+++ ManagerClient/src/com/tm/MessagePane.java
@@ -1,15 +1,24 @@
 package com.tm;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MessagePane extends JPanel {
 
     private final ManagerClient client;
     private final String login;
 
+    private DefaultListModel<String> listModel = new DefaultListModel<>();
+    private JList<String> messageList = new JList<>(listModel);
+    private JTextField inputField = new JTextField();
 
     public MessagePane(ManagerClient client, String login) {
         this.client = client;
         this.login = login;
+
+        setLayout(new BorderLayout());
+        add(new JScrollPane(messageList), BorderLayout.CENTER);
+        add(inputField, BorderLayout.SOUTH);
+
     }
 }
