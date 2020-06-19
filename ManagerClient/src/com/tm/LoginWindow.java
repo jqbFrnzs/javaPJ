43c7f241d1d94d43b86a8ab5cc21e80a7325bab7
Jqb Frnzs <jacobFz@tutanota.com>
2020-06-19 16:14:31 +0200

    Added layout of login window and its display method

--- ManagerClient/src/com/tm/LoginWindow.java
+++ ManagerClient/src/com/tm/LoginWindow.java
@@ -1,6 +1,7 @@
 package com.tm;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginWindow extends JFrame {
     JTextField loginField = new JTextField();
@@ -9,5 +10,25 @@ public class LoginWindow extends JFrame {
 
     public LoginWindow() {
         super("Login");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        p.add(loginField);
+        p.add(passwordField);
+        p.add(loginButton);
+
+        getContentPane().add(p, BorderLayout.CENTER);
+
+        pack();
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        LoginWindow loginWin = new LoginWindow();
+        loginWin.setVisible(true);
+
     }
 }
